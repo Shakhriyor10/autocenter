ce98a4f4dee9b30011a6a1df858679e9a873cf5e
Shakhriyor10 <77412906+Shakhriyor10@users.noreply.github.com>
2026-03-17 11:38:56 +0500

    Показывать 8 самых дорогих и popular топ-продаж в нужных блоках

--- frontend/views.py
+++ frontend/views.py
@@ -4,5 +4,15 @@ from frontend.models import Car
 
 
 def index(request):
-    cars = Car.objects.select_related("brand")
-    return render(request, "index4.html", {"cars": cars})
+    expensive_cars = Car.objects.select_related("brand").order_by("-price")[:8]
+    popular_vehicles = (
+        Car.objects.select_related("brand").filter(is_hot=True).order_by("-price")[:8]
+    )
+    return render(
+        request,
+        "index4.html",
+        {
+            "expensive_cars": expensive_cars,
+            "popular_vehicles": popular_vehicles,
+        },
+    )
