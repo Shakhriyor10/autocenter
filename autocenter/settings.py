d546308e3b02569e57f35f0e3f350e706481812e
Shakhriyor10 <nnnnafisa@gmail.com>
2026-03-17 10:54:33 +0500

    sldskdsakdasdk

--- autocenter/settings.py
+++ autocenter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-imqodyzx5^19q4tj9n(nj@hj-@xkmjx=r*$3@j!)qmlrzoytzq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -118,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_DIR = BASE_DIR / 'static'
-# STATICFILES_DIRS = [
-#     STATIC_DIR
-# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
 
 
 MEDIA_URL = '/media/'
